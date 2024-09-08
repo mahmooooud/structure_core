e2fa6da4595ee04920bee4e4aaaac1c51cac02a5
Mahmoud Ahmed <ebrahimahmed2246@gmail.com>
2024-09-08 13:52:59 +0300

    edits

--- lib/core/di/service_locator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-
-import 'service_locator.config.dart';
-
-@InjectableInit()
-Future<void> configureDependencies() async => GetItInjectableX(GetIt.I).init();
