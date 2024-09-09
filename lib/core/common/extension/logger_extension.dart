a8fd361a1cf148192f4a211da4da65922edde6ff
Mahmoud Ahmed <ebrahimahmed2246@gmail.com>
2024-09-09 14:37:30 +0300

    edits

---
+++ lib/core/common/extension/logger_extension.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+
+extension loggerExtension on dynamic {
+  void get logE => GetIt.I<Logger>().e(this);
+
+  void get logW => GetIt.I<Logger>().w(this);
+
+  void get logI => GetIt.I<Logger>().i(this);
+}
