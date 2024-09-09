9f16af4cdd00bdaa6293667237d49a464259f728
Mahmoud Ahmed <ebrahimahmed2246@gmail.com>
2024-09-09 14:46:34 +0300

    edits

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -3,44 +3,44 @@ import 'package:flutter/material.dart';
 
 
 class ApplicationConstants {
-  static const TITLE_APP_NAME = 'ABH';
-  static const KEY_LANGUAGE = 'language';
-  static const KEY_TOKEN = 'token';
-  static const HEADER_BEARER = 'bearer';
-  static const ID_TOKEN = 'idToken';
-  static const USER_DATA = 'userData';
-  static const Guard_DATA = 'guardData';
-  static const FONT_SIZE = 'font-size';
-  static const REFRESH_TOKEN = 'refresh_token';
-  static const KEY_FIREBASE_TOKEN = 'firebase_token';
-  static const KEY_FIRST_START = 'First_start';
-  static const KEY_IS_Guard = 'is_guard';
-  static const KEY_IS_CHECKED_IN = 'is_checked_in';
-  static const KEY_IS_CHECKED_OUT = 'is_checked_out';
-  static const KEY_MAX_CHECK_IN = 'max_time_to_checkInt';
-  static const KEY_CHECKED_DATE = 'checked_date';
-  static const IS_ANONYMOUS_TOKEN = 'IsAnonymousToken';
-  static const OFFLINE_REQUESTS = 'offline_requests';
+  String TITLE_APP_NAME = 'ABH';
+  String KEY_LANGUAGE = 'language';
+  String KEY_TOKEN = 'token';
+  String HEADER_BEARER = 'bearer';
+  String ID_TOKEN = 'idToken';
+  String USER_DATA = 'userData';
+  String Guard_DATA = 'guardData';
+  String FONT_SIZE = 'font-size';
+  String REFRESH_TOKEN = 'refresh_token';
+  String KEY_FIREBASE_TOKEN = 'firebase_token';
+  String KEY_FIRST_START = 'First_start';
+  String KEY_IS_Guard = 'is_guard';
+  String KEY_IS_CHECKED_IN = 'is_checked_in';
+  String KEY_IS_CHECKED_OUT = 'is_checked_out';
+  String KEY_MAX_CHECK_IN = 'max_time_to_checkInt';
+  String KEY_CHECKED_DATE = 'checked_date';
+  String IS_ANONYMOUS_TOKEN = 'IsAnonymousToken';
+  String OFFLINE_REQUESTS = 'offline_requests';
 
-  static const FONT_FAMILY = 'ABH';
-  static const FONT_FAMILY_URL =
+  String FONT_FAMILY = 'ABH';
+  String FONT_FAMILY_URL =
       'https://cdn.jsdelivr.net/gh/googlefonts/roboto@v2.138/src/hinted/Roboto-Regular.ttf';
-  static const ICO_FONT_FAMILY_ICONS = 'common';
-  static const Prime_FONT_FAMILY_ICONS = 'primeicons';
-  static const FONT_FAMILY_ICON_URL =
+  String ICO_FONT_FAMILY_ICONS = 'common';
+  String Prime_FONT_FAMILY_ICONS = 'primeicons';
+  String FONT_FAMILY_ICON_URL =
       'https://dev.saeed.projects.roaa.tech/assets/fonts/icomoon/icomoon.ttf?iefix';
 
-  static const authHeader = 'Authorization';
-  static const imageLogo = "assets/images/osos.png";
+  String authHeader = 'Authorization';
+  String imageLogo = "assets/images/osos.png";
 
 // Languages
-  static const langEN = 'en';
-  static const langAR = 'ar';
-  static const lang = 'lang';
+  String langEN = 'en';
+  String langAR = 'ar';
+  String lang = 'lang';
 
   /// Pagination
-  static const paginationLimit = 16;
-  static const paginationSkip = 0;
+  int paginationLimit = 16;
+  int paginationSkip = 0;
 
-  static const BorderRadius appBorderRadius = BorderRadius.all(Radius.circular(5));
+  BorderRadius appBorderRadius = BorderRadius.all(Radius.circular(5));
 }
